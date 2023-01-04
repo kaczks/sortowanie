896862a163d8ece18667dc1adc646ba288146740
IETI <58682773+marcinkolacz@users.noreply.github.com>
2023-01-04 09:57:52 +0100

    losowanie liczb ktore nie wiem czy działa

--- app/src/main/java/com/example/sortowanie/MainActivity.kt
+++ app/src/main/java/com/example/sortowanie/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.sortowanie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edit = findViewById<EditText>(R.id.liczbyED)
+
+        findViewById<Button>(R.id.sortujBtn).setOnClickListener {
+            val rand = Random()
+            for (int in 1 .. edit.text.toString().toInt() ) {
+                val a = rand.nextInt(edit.text.toString().toInt())
+                findViewById<TextView>(R.id.WpyiszTW).append(a.toString())
+            }
+        }
     }
 }
